9ea7b41a21d128d40779f1f5fd1195c32a2a5c80
Elodie Meunier <elow.m@live.fr>
2019-03-19 16:44:03 +0100

    Création du menu en responsive design

---
+++ public/js/menu.js
@@ -0,0 +1,13 @@
+// Menu Hamburger
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('btn-nav').addEventListener('click', () => {
+        const openClass = 'open';
+        const openNav = document.getElementById('menu');
+
+        if (openNav.classList.contains(openClass) === true) {
+            openNav.classList.remove(openClass);
+        } else {
+            openNav.classList.add(openClass);
+        }
+    });
+});

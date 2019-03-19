c7761671b2ab09e3f6341c40e2283a47e5133b3c
Elodie Meunier <elow.m@live.fr>
2019-03-19 16:37:04 +0100

    Ajout des actions dans le routeur pour les chapitres et les commentaires de la page "Chapitres"

--- index.php
+++ index.php
@@ -6,6 +6,20 @@ try {
     if (isset($_GET['action'])) {
         if ($_GET['action'] == 'resumeChapter') {
             resumeChapter();
+        } elseif ($_GET['action'] == 'allChapters') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                allChapters();
+            }
+        } elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                } else {
+                    throw new Exception('Tous les champs ne sont pas remplis');
+                }
+            } else {
+                throw new Exception('Aucun identifiant de chapitre envoyé.');
+            }
         } elseif ($_GET['action'] == 'connected') {
             connected();
         } elseif ($_GET['action'] == 'contact') {

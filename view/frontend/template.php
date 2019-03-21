0ea4465b1dca3ce67f50ab6714c3590c63eef2db
Elodie Meunier <elow.m@live.fr>
2019-03-21 17:05:11 +0100

    abréviation echo pour la feuille de style css

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="<?php echo CSS ?>style.css">
+    <link rel="stylesheet" href="<?= CSS ?>style.css">
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Kalam:300,400,700|Raleway" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
@@ -20,7 +20,7 @@
         </div>
         <nav id="menu">
             <ul>
-                <li><a href="/index.php"><i class="fas fa-igloo"></i> Accueil</li></a>
+                <li><a href="index.php"><i class="fas fa-igloo"></i> Accueil</li></a>
                 <li><a href="index.php?action=allChapters&id=1"><i class="fas fa-book"></i> Chapitres</li></a>
                 <li><a href="index.php?action=contact"><i class="fas fa-mobile"></i> Contact</li></a>
                 <li><a href="index.php?action=connected"><i class="fas fa-sign-in-alt"></i> Connexion</li></a>

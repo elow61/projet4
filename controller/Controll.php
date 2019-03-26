86d1ef465f80606569639328a8f823cf0721ccb1
Elodie Meunier <elow.m@live.fr>
2019-03-26 18:36:11 +0100

    Class du controller (accès utilisateur)

---
+++ controller/Controll.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Elodie\Projet4\Controller;
+
+// Chargement des classes
+require_once('model/ChaptersManager.php');
+require_once('model/CommentManager.php');
+
+class Controll {
+    
+    // chapitres contenus sur la page d'accueil
+    public function resumeChapter() {
+        $chapterManager = new \Elodie\Projet4\Model\ChaptersManager();
+        $chapters = $chapterManager->getChapters();
+
+        require(VIEW.'frontend/home.php');
+    }
+
+    // Chapitres contenus sur la page Chapitre
+    public function allChapters() {
+        $chapterManager = new \Elodie\Projet4\Model\ChaptersManager();
+        $commentManager = new \Elodie\Projet4\Model\CommentManager();
+
+        $chapter_single = $chapterManager->getChapter($_GET['id']);
+
+        $chapters = $chapterManager->totalChapters();
+        $comments = $commentManager->getComments($_GET['id']);
+
+        // vérifie que l'ID du chapitre existe bien
+        if (!empty($chapter_single)) {
+            require(VIEW.'frontend/chapters.php');
+        } else {
+            header('Location: index.php');
+        }
+    }
+
+    // Récupération des commentaires pour les ajouter en base
+    public function addComment($chapterId, $author, $comment) {
+        $commentManager = new \Elodie\Projet4\Model\CommentManager();
+
+        $affectedLines = $commentManager->addToComment($chapterId, $author, $comment);
+
+        if ($affectedLines === false) {
+            throw new Exception('Impossible d\'ajouter le commentaire');
+        } else {
+            header('Location: index.php?action=allChapters&id=' . $chapterId);
+        }
+    }
+
+    // Gestion de la page Connexion
+    public function connected() {
+        require(VIEW.'frontend/connexion.php');
+    }
+}

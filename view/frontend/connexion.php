7440a15e6a16ef318897f8a83ad59e51adde73ca
Elodie Meunier <elow.m@live.fr>
2019-04-14 19:33:49 +0200

    MAJ style page CONNEXION

--- view/frontend/connexion.php
+++ view/frontend/connexion.php
@@ -1,29 +1,19 @@
 <?php
 $title = "Espace Membre"; ?>
 <?php ob_start(); ?>
-<?php
-
-// echo '<pre>';
-// print_r($_SESSION);
-// echo '</pre>';
-?>
-<section>
-    <div class="message-connexion">
-        <p>Espace réservé aux administrateurs.</p>
-    </div>
-</section>
+<div class="bg-head">
+    <p class="mess-head">Espace réservé aux administrateurs.</p>
+</div>
 
 <section id="form-connect">
     <div class="container-form-connect">
         <h2>Se connecter</h2>
             <form action="index.php?action=auth" method="post">
                 <p> 
-                    <label for="pseudo">Pseudo : </label>
                     <input type="text" name="pseudo" placeholder="Nom d'utilisateur">
                 </p>
                 <p>
-                    <label for="mdp">Mot de passe : </label>
-                    <input type="password" name="mdp" placeholder="Votre mot de passe">
+                    <input type="password" name="mdp" placeholder="Mot de passe">
                 </p>
                 <button type="submit">Se connecter</button>
             </form>

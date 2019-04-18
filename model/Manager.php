3cbbc047279cf0be70f162bc316adddb82db6d60
Elodie Meunier <elow.m@live.fr>
2019-04-18 21:06:07 +0200

    Retrait de try / catch dans la base de donnée puisque l'index l'éxécute déjà

--- model/Manager.php
+++ model/Manager.php
@@ -6,14 +6,9 @@ class Manager {
     private $db;
 
     protected function dbConnect() {
-        try {
-            $this->db = new \PDO('mysql:host=localhost;dbname=projet4', 'root', 'root', 
-            array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
-        } catch (Exception $e) {
-            echo 'Ligne : ' .$e->getLine() . '<br />';  
-            die('Erreur : ' .$e->getMessage()) . '<br />';
-            echo 'La base de donnée n\'est pas disponible pour le moment.';
-        }
+
+        $this->db = new \PDO('mysql:host=localhost;dbname=projet4', 'root', 'root', 
+        array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
         return $this->db;
     }
 }

6a533c8ba043e4fa66335893ecde94db2de56117
Elodie Meunier <elow.m@live.fr>
2019-04-24 09:53:07 +0200

    Ajout d'une seule police pour la page d'Erreur.

--- view/frontend/errorView.php
+++ view/frontend/errorView.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Erreur</title>
     <link rel="stylesheet" href="<?= CSS ?>style.css">
-    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Kalam:300,400,700|Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
 
